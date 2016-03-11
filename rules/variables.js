3d0d24752ac7c867606241b969fcc1e5fab8932b
Tom Adam Shani <tom@tomshani.com>
2016-03-11 03:03:53 -0500

    set no-undef from warning to error

--- rules/variables.js
+++ rules/variables.js
@@ -15,7 +15,7 @@ module.exports = {
     // disallow use of undefined when initializing variables
     'no-undef-init': 0,
     // disallow use of undeclared variables unless mentioned in a /*global */ block
-    'no-undef': 1,
+    'no-undef': 2,
     // disallow use of undefined variable
     'no-undefined': 0,
     // disallow declaration of variables that are not used in the code
