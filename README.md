24c31ddde493b577512056d3fa1f84201456cc18
Tom Adam Shani <tom@tomshani.com>
2017-08-01 02:29:03 -0400

    v2.0.0

--- README.md
+++
@@ -1,8 +0,0 @@
-# eslint-config-pomo
-
-Based on eslint-config-airbnb@10.0.0. I use this every day, but it is completely untested; use at your own risk.
-
-## Usage
-
-1. `npm install --save-dev eslint-config-pomo eslint-plugin-react eslint`
-2. add `"extends": "pomo"` to your .eslintrc
--- index.js
+++ index.js
@@ -1,35 +1,7 @@
 module.exports = {
   extends: [
-    './rules/base',
-    './rules/react',
-    './rules/react-a11y'
-  ].map(require.resolve),
-
-  plugins: [
-    'import'
+    'airbnb',
+    'pomo-base'
   ],
-
-  settings: {
-    'import/resolver': { node: { extensions: ['.js', '.json'] } },
-    'import/extensions': [
-      '.js',
-      '.jsx'
-    ],
-    'import/core-modules': [
-    ],
-    'import/ignore': [
-      'node_modules',
-      '\\.(coffee|scss|css|less|hbs|svg|json)$'
-    ]
-  },
-
-  env: {
-    es6: true,
-    node: true
-  },
-
-  parserOptions: {
-    ecmaVersion: 7,
-    sourceType: 'module'
-  }
-};
+  rules: {}
+}
